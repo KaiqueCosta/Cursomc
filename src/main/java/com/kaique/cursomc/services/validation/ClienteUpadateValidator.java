b1adef714854e944e7b9304fab5a8dd16cd3b431
Kaique Tokunbo Costa Sanni <kaiquesanni@brq.com>
2020-02-28 10:34:20 -0300

    Subindo Projeto

---
+++ src/main/java/com/kaique/cursomc/services/validation/ClienteUpadateValidator.java
@@ -0,0 +1,56 @@
+package com.kaique.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kaique.cursomc.domain.Cliente;
+import com.kaique.cursomc.dto.ClienteNewDTO;
+import com.kaique.cursomc.enums.TipoCliente;
+import com.kaique.cursomc.repositories.ClienteRepository;
+import com.kaique.cursomc.resources.exception.FieldMessage;
+import com.kaique.cursomc.services.validation.utils.BR;
+
+public class ClienteUpadateValidator implements ConstraintValidator<ClienteUpdate, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if(objDto.getTipo() == null) {
+			list.add(new FieldMessage("tipo", "Tipo nao pode ser nulo"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF invalido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
