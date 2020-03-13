2bf8842178423f7cbf475a7fd971cd204f5cc92c
Kaique Tokunbo Costa Sanni <kaiquesanni@brq.com>
2020-03-13 15:36:31 -0300

    atualização do projeto

--- src/main/java/com/kaique/cursomc/services/DBService.java
+++ src/main/java/com/kaique/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.kaique.cursomc.domain.PagamentoComCartao;
 import com.kaique.cursomc.domain.Pedido;
 import com.kaique.cursomc.domain.Produto;
 import com.kaique.cursomc.enums.EstadoPagamento;
+import com.kaique.cursomc.enums.Perfil;
 import com.kaique.cursomc.enums.TipoCliente;
 import com.kaique.cursomc.repositories.CategoriaRepository;
 import com.kaique.cursomc.repositories.CidadeRepository;
@@ -212,11 +213,11 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		Cliente cli1 = new Cliente(null, "Kaique", "kaique.tcs2001@gmail.com", "36378912377", TipoCliente.PESSOAFISICA,pe.encode( "123"));
-		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
 		Cliente cli2 = new Cliente(null, "Ana Costa", "nelio.iftm@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
 		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
