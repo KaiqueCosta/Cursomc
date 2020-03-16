c9d1abee7f3e419283efd6d10aff3ba255d385dc
Kaique Tokunbo Costa Sanni <kaiquesanni@brq.com>
2020-03-16 09:55:25 -0300

    implementacao e geracao de token JWT

---
+++ src/main/java/com/kaique/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.kaique.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.kaique.cursomc.domain.Cliente;
+import com.kaique.cursomc.repositories.ClienteRepository;
+import com.kaique.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
