08b56ecb849255e65d3ea15b3639e580f077e19b
Kaique Tokunbo Costa Sanni <kaiquesanni@brq.com>
2020-03-23 11:43:34 -0300

    esqueci a senha

--- src/main/java/com/kaique/cursomc/services/PedidoService.java
+++ src/main/java/com/kaique/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.kaique.cursomc.domain.Cliente;
 import com.kaique.cursomc.domain.ItemPedido;
 import com.kaique.cursomc.domain.PagamentoComBoleto;
 import com.kaique.cursomc.domain.Pedido;
@@ -13,38 +17,40 @@ import com.kaique.cursomc.enums.EstadoPagamento;
 import com.kaique.cursomc.repositories.ItemPedidoRepository;
 import com.kaique.cursomc.repositories.PagamentoRepository;
 import com.kaique.cursomc.repositories.PedidoRepository;
+import com.kaique.cursomc.security.UserSS;
+import com.kaique.cursomc.services.exceptions.AuthorizationException;
 import com.kaique.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-
+	
 	@Autowired
 	private PedidoRepository repo;
-
+	
 	@Autowired
 	private BoletoService boletoService;
-
+	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-
+	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-
+	
 	@Autowired
 	private ProdutoService produtoService;
-
+	
 	@Autowired
 	private ClienteService clienteService;
-
+	
 	@Autowired
 	private EmailService emailService;
-
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-
+	
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
@@ -67,5 +73,14 @@ public class PedidoService {
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
-
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
